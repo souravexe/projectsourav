238a91c92800d046f9d1005d7bd02028619e25ea
Sourav Kumar <souravkumar72178@gmail.com>
2026-03-20 15:07:57 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,11 @@
+##calculator
+# a=3
+# b=5
+# print("The value of",a,"+",b,"is",a+b)
+# print("The value of",a,"-",b,"is",a-b)
+# print("The value of",a,"*",b,"is",a*b)
+# print("The value of",a,"/",b,"is",a/b)
+# print("The value of",a,"%",b,"is",a%b)
+# print("The value of",a,"**",b,"is",a**b)
+
+
